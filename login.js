8925e7742e588091bcaf26b12c8d9b3ab5fb37d5
anim710 <anim3654@gmail.com>
2026-03-09 09:42:48 +0600

    added log credential and js file

---
+++ login.js
@@ -0,0 +1,24 @@
+document.getElementById('sign-in-btn').addEventListener('click', function(event) {
+
+    event.preventDefault();
+    const userName=document.getElementById('username').value;
+    console.log(userName);
+    const passWord=document.getElementById('password').value;
+    // const convertedPinNumber=parseInt(pinNumber)
+    // console.log(convertedPinNumber);
+    if(userName.length===11){
+        if(passWord=== "admin123"){
+             alert("Login Successful");
+            window.location.href="./main.html";
+
+    }
+    else{
+        alert("Incorrect Password");
+    }
+}
+    else{
+        alert("Username must be 11 characters");
+    }
+
+    
+});
